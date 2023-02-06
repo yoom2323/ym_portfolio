88262e74b4ca0e33cdaba044bfcb0b5ca656f02d
yoom2323 <omgvic@naver.com>
2023-02-06 22:06:07 +0900

    메인컨 수정

--- src/components/Maincon.js
+++ src/components/Maincon.js
@@ -18,20 +18,24 @@ const pfData = [
     id: 1,
     title: pf1,
     img: <img src={pf1} alt="pf1" />,
-    des: "Studio RHE",
+    des: <span>Studio RHE</span>,
+    des2: <span>#가로스크롤<br/>#스크롤 인디케이터<br/>#영상 임베드<br/>#팀 프로젝트</span>
   },
-  { id: 2, title: pf2, img: <img src={pf2} alt="pf2" />, des: "desdesdes" },
+  { id: 2, title: pf2, img: <img src={pf2} alt="pf2" />,     des: <span>DYAMI</span>,
+  des2: <span>#리액트<br/>#스와이퍼<br/>#OpenAPI 활용<br/>#개인 작업</span> },
   {
     id: 3,
     title: pf2,
     img: <img src="" alt="pf3" />,
-    des: "pf3pf3pf3pf3pf3pf3pf3",
+    des: <span>Pf3</span>,
+  des2: <span>#리액트<br/>#스와이퍼<br/>#OpenAPI 활용<br/>#개인 작업</span>
   },
   {
     id: 4,
     title: pf2,
     img: <img src="" alt="pf4" />,
-    des: "pf4pf4pf4pf4pf4pf4pf4pf4",
+    des: <span>Pf4</span>,
+  des2: <span>#리액트<br/>#스와이퍼<br/>#OpenAPI 활용<br/>#개인 작업</span>
   },
 ];
 
@@ -40,11 +44,13 @@ const pfData = [
 }
 function Maincon(props) {
   const [text, setText] = useState("");
+  const [text2, setText2] = useState("");
   const [img, setImg] = useState("");
 
   const des = (i) => {
     const result = pfData.findIndex((item) => item.id === i);
     setText(pfData[result].des);
+    setText2(pfData[result].des2);
   };
 
   return (
@@ -97,7 +103,7 @@ function Maincon(props) {
             to="/Pf1"
             style={{ textDecoration: "none" }}
             onMouseEnter={() => [des(pfData[0].id)]}
-            onMouseLeave={() => setText("")}
+            onMouseLeave={() => [setText(""), setText2("")]}
           >
             {pfData[0].img}
           </Link>
@@ -105,13 +111,13 @@ function Maincon(props) {
         <div className="item7 tree">
           <img src={tree} alt="tree" />
         </div>
-        <div className="item8 description">{text}</div>
+        <div className="item8 description"><p>{text}</p>{text2}</div>
         <div className="item9 pj2">
           <Link
             to="/Pf2"
             style={{ textDecoration: "none" }}
             onMouseEnter={() => [des(pfData[1].id)]}
-            onMouseLeave={() => setText("")}
+            onMouseLeave={() => [setText(""), setText2("")]}
           >
             {pfData[1].img}
           </Link>
@@ -121,7 +127,7 @@ function Maincon(props) {
             to="/Pf3"
             style={{ textDecoration: "none" }}
             onMouseEnter={() => [des(pfData[2].id)]}
-            onMouseLeave={() => setText("")}
+            onMouseLeave={() => [setText(""), setText2("")]}
           >
             {pfData[2].img}
           </Link>
@@ -134,7 +140,7 @@ function Maincon(props) {
             to="/Pf4"
             style={{ textDecoration: "none" }}
             onMouseEnter={() => [des(pfData[3].id)]}
-            onMouseLeave={() => setText("")}
+            onMouseLeave={() => [setText(""), setText2("")]}
           >
             {pfData[3].img}
           </Link>
